14fab4650b17fc3d126b049cc3a6616b2134c4ac
Rishikesh Salunke <12626@ceinsys.com>
2026-03-12 16:35:16 +0530

    fix(server): support iisnode port/pipe and health check

--- server/index.ts
+++ server/index.ts
@@ -168,12 +168,12 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
+  // iisnode requires listening on process.env.PORT
+  const port = process.env.PORT || 5000;
   server.listen(
     {
       port,
       host: "0.0.0.0",
-      // reusePort: true,
     },
     () => {
       log(`serving on port ${port}`);
